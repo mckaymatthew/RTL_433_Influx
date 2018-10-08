3584eac16629032a3776a015612385c58b24b986
mckaym <matt@mattmckay.org>
2018-10-07 19:34:38 -0500

    Parameterizing host

--- influxposter.cpp
+++ influxposter.cpp
@@ -8,7 +8,6 @@ InfluxPoster::InfluxPoster(QObject *parent)
     , manager(this)
     , m_settings("map.ini", QSettings::IniFormat)
 {
-    m_settings.setValue("Hoooo","");
     m_settings.sync();
     QObject::connect(&manager, &QNetworkAccessManager::finished,
         this, [=](QNetworkReply *reply) {
@@ -30,8 +29,7 @@ void InfluxPoster::processTempMeasurement(int16_t id, double tempC) {
     if(m_settings.contains(lut)) {
         QString idStr = m_settings.value(lut).toString();
         QString postData = QString("temp,room=%1 value=%2").arg(idStr).arg(tempC);
-
-        QNetworkRequest request(QUrl("http://10.0.0.23:8086/write?db=temp"));
+        QNetworkRequest request(QUrl("http://"+m_settings.value("host").toString()+":8086/write?db=temp"));
         request.setHeader(QNetworkRequest::ContentTypeHeader,"application/x-www-form-urlencoded");
         manager.post(request, postData.toUtf8());
 
@@ -44,7 +42,7 @@ void InfluxPoster::processHumidityMeasurement(int16_t id, double relH) {
         QString idStr = m_settings.value(lut).toString();
         QString postData = QString("humidity,room=%1 value=%2").arg(idStr).arg(relH);
 
-        QNetworkRequest request(QUrl("http://10.0.0.23:8086/write?db=humid"));
+        QNetworkRequest request(QUrl("http://"+m_settings.value("host").toString()+":8086/write?db=humid"));
         request.setHeader(QNetworkRequest::ContentTypeHeader,"application/x-www-form-urlencoded");
         manager.post(request, postData.toUtf8());
 
